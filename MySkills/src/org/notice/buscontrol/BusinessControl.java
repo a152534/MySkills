15132219c323fcf38039d140a6658c95a56758d6
brad <brad@brad-ThinkPad-P50>
2018-01-18 10:47:21 +0200

    BusinessControlV1

---
+++ MySkills/src/org/notice/buscontrol/BusinessControl.java
@@ -0,0 +1,101 @@
+package org.notice.buscontrol;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import org.notice.beans.User;
+import org.notice.dao.MySkillsDAO;
+
+public class BusinessControl
+{
+    private String userId;
+    private MySkillsDAO skillsDB  = null;
+    private ResultSet RS = null;
+    
+    private MySkillsDAO userAccess = null;
+    private ArrayList<User> UserList = null;
+	private Connection userConnect = null;
+	private Statement Usertate = null;
+	private ResultSet userResult = null;
+   
+    public BusinessControl()
+    {
+
+    }
+    
+    public boolean ValidateUser(String userId)
+    {
+        this.userId = userId;
+        try
+        {
+    	skillsDB = new MySkillsDAO();
+    	RS = skillsDB.queryDB("select * from user where user_id = '" + userId + "'");
+    	if (!RS.next())
+    	{
+    		
+    		System.out.println("user does not exist");
+    		return false;
+    	}
+    	else 
+    	{
+    	    
+    	    return true;
+    	}
+        } 
+        	catch (SQLException e)
+        {
+    	e.printStackTrace();
+    	return false;
+        } 
+        catch (Exception e)
+        {
+    	// TODO Auto-generated catch block
+    	e.printStackTrace();
+    	return false;
+        }
+      
+        
+        
+    }
+    
+    public ArrayList<User> getUser(String userId)
+	{
+		UserList = new ArrayList<User>();
+		this.userId = userId;
+		if ValidateUser(userId)
+		{
+		
+		try
+		{
+			//Fetch from database
+			
+			userResult = skillsDB.queryDB("select * from user where user_id like '" + user_ID + "%'" +
+					 " or first_name like '" + user_ID +  "%' or surname like '" + user_ID + "%'");
+			
+			//Write to ArrayList
+			String userID = userResult.getString("user_id");
+			String firstName = userResult.getString("first_name");
+			String surname = userResult.getString("surname");
+			String aliasName = userResult.getString("alias_name");
+			String email = userResult.getString("email");
+			String phoneNum = userResult.getString("phone_num");
+			 
+			UserList.add(new User(userID, firstName, surname, aliasName, email, phoneNum));
+		} catch (SQLException se)
+		{
+			System.out.println("ERROR: " + se.getMessage());
+			return null;
+		}
+		return UserList;
+		}
+		else
+		{
+		    UserList= null;
+		    
+		}
+	}
+    
+}
