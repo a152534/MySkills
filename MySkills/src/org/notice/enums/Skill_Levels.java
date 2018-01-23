002ae960a1476b623ca6112558421f645be3dd5c
A150301 <A150301@26413JNBPBB506L.za.sbicdirectory.com>
2018-01-23 14:38:48 +0200

    Updated Skill Level Enums

--- MySkills/src/org/notice/enums/Skill_Levels.java
+++ MySkills/src/org/notice/enums/Skill_Levels.java
@@ -2,9 +2,16 @@ package org.notice.enums;
 
 public enum Skill_Levels
 {
-	Notice,
-	Advanced_Beginner,
-	Competentt,
-	Proficien,
-	Expert;
+	Notice(1),
+	Advanced_Beginner(2),
+	Competent(3),
+	Proficient(4),
+	Expert(5);
+	
+	private int value;
+	
+	private Skill_Levels(int value)
+	{
+		this.value = value;
+	}
 }
