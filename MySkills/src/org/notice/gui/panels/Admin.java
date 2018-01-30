7521b0a298421656d965a07a0eb2607b6298ded9
A044339 <A044339@21700JNBPBB111L.za.sbicdirectory.com>
2018-01-30 14:32:22 +0200

    Admin

--- MySkills/src/org/notice/gui/panels/Admin.java
+++ MySkills/src/org/notice/gui/panels/Admin.java
@@ -1,76 +1,51 @@
 package org.notice.gui.panels;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.TableColumn;
 
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Vector;
 
-import org.notice.beans.*;
+import java.util.ArrayList;
+
+
+import org.notice.beans.Skill;
+
 import org.notice.buscontrol.*;
+import org.notice.beans.CommonStuff;
 import org.notice.client.Transaction;
-import org.notice.*;
-import org.notice.client.Transaction;
-import org.notice.tablemodel.MyProfileRatedSkillTableModel;
-import java.awt.Button;
+import org.notice.gui.panels.SkillSelector;
+import org.notice.tablemodel.SkillsRatedTableModel;
+import org.notice.tablemodel.SkillsUtilTableModel;
+
 
 public class Admin extends JPanel implements ActionListener
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JTable tableSkills;
-	private JScrollPane scrollPaneSkills;
 	private JButton btnAdd;
 	private JButton btnDelete;
+	private Font fontButton;
 	private CommonStuff commonStuff;
+	private SkillsUtilTableModel myModel ;
 	private Transaction transaction;
-	private static JButton btnYes;
-	private JButton btnNo;
-	private BusinessControl businessControl;
-	private String deleteSkill;
-	private int skillID;
-	private String dulicateSkill;
-	private JComboBox<String> comboBoxSearch = null;
-	private String skillName;
 	private SkillSelector skillSelector;
 	private ArrayList<Skill> skills;
-	private MyProfileRatedSkillTableModel myModel;
-	private Font fontLabel;
-	private Font fontButton;
-	private Font fontTextArea;
-	private Font fontTextBox;
-	private Font fontComboBox;
-
+	
 	public Admin(CommonStuff inCommonStuff)
 	{
 		// skillName = ("PetraTest");
 		// skillID = 51517;
 
-		setLayout(null);
-		
 		commonStuff = inCommonStuff;
-		
-		fontLabel = commonStuff.getFontLabel();
-		fontButton = commonStuff.getFontButton();
-		fontTextArea = commonStuff.getFontTextArea();
-		fontTextBox = commonStuff.getFontTextBox();
-		fontComboBox = commonStuff.getFontComboBox();
 
-//		fontButton = (new Font("Arial", Font.BOLD, 18));
-//		fontTextArea = (new Font("Arial", Font.PLAIN, 14));
-//		fontComboBoxSearch = (new Font("Arial", Font.PLAIN, 14));
+		setLayout(null);
 
-		skillSelector = new SkillSelector(commonStuff.getSkillsList());
-		skillSelector.setBounds(100, 32, 620, 500);
-		add(skillSelector);
-
+		fontButton = (new Font("Arial", Font.BOLD, 18));
+	
 		btnDelete = new JButton("Delete");
 		btnDelete.setEnabled(true);
 		//btnDelete.setEnabled(false);
@@ -87,38 +62,68 @@ public class Admin extends JPanel implements ActionListener
 		btnAdd.setFont(fontButton);
 		btnAdd.setBounds(259, 532, 100, 25);
 		add(btnAdd);
+		
+		skillSelector = new SkillSelector(commonStuff.getSkillsList());
+		skillSelector.setBounds(100, 32, 620, 400);
+		
+		skills = new ArrayList<Skill>();
+	
 
-		// populateSkillsList();
+	
+		skillSelector.getSkillModel().fireTableDataChanged();
+		 
+ 		 
+		
+		//myModel.fireTableDataChanged();
+		 
+
+		 populateSkillsList();
 	}
 
 	private void populateSkillsList()
 	{
-		// ArrayList<Skill> skillsList = new ArrayList<Skill> ();
-		//
+		
+ 
+		
+		
 		transaction = new Transaction("getSkillList", null);
+ 		
 		transaction = commonStuff.getClient().sendTransaction(transaction);
-
-		commonStuff.setSkillsList((ArrayList<Skill>) transaction.getObject());
-		//
-		// for(int pos = 0; pos < skillsList.size() -1 ; pos++)
-		// {
-		// comboBoxSearch.addItem(skillsList.get(pos).getSkillName());
-		// }
+		ArrayList<Skill> skills = (ArrayList<Skill>)transaction.getObject();
+	 		
+	    System.out.println("SKILLS  "  + skills.size());
+	    skillSelector.getSkillModel().setSkills(skills);
+		//skillSelector = new SkillSelector(commonStuff.getSkillsList());
+		 
+		skillSelector.setBounds(100, 32, 620, 500);
+		add(skillSelector);
+		System.out.println("After add skillSelector " );
 	}
+	
 
 	private void refreshSkills()
 	{
-		transaction = new Transaction("getUserSkills", commonStuff.getLoggedOnUser().getUserID());
+		transaction = new Transaction("getSkillList", null);
 		transaction = commonStuff.getClient().sendTransaction(transaction);
-		ArrayList<Skill> skillsList = (ArrayList<Skill>) transaction.getObject();
-
-		skills.clear();
-		for (Skill skill : skillsList)
-		{
-			skills.add(skill);
-		}
-		myModel.fireTableDataChanged();
+		ArrayList<Skill> newSkillList = (ArrayList<Skill>)transaction.getObject();
+	    commonStuff.setSkillsList(newSkillList);
+	    skillSelector.getSkillModel().setSkills(newSkillList);
+	
+	//	skillSelector = new SkillSelector(commonStuff.getSkillsList());
 		
+		
+		System.out.println("Before SKILLS CLEAR");	
+	 	
+	// 	skills.clear();  
+	//	for (Skill skill : newSkillList)
+	//	{
+	//		skills.add(skill);
+	//	}
+//		skillSelector.setSkillModel(myModel);
+//		 
+//		myModel.fireTableDataChanged();
+		skillSelector.getSkillModel().fireTableDataChanged();
+		//validate();
 	}
 
 	@Override
@@ -140,16 +145,17 @@ public class Admin extends JPanel implements ActionListener
 
 			if (dialogResult == JOptionPane.YES_OPTION)
 			{
-				transaction = new Transaction("DeleteSkill", removeSkill.getSkillID()); // setup transaction
-				System.out.println("Delete SkillID: " + removeSkill.getSkillID());
-				transaction = commonStuff.getClient().sendTransaction(transaction); // sent transaction
+				transaction = new Transaction("DeleteSkill", removeSkill.getSkillID()); // setup
+																		// transaction
+			//	System.out.println("Delete SkillID: " + removeSkill.getSkillID());
+				transaction = commonStuff.getClient().sendTransaction(transaction); // sent
+																					// transaction
 				successful = (boolean) transaction.getObject();
 				
 				if(successful)
 				{
 					JOptionPane.showMessageDialog(null, "Skill has been successfully deleted");
-					populateSkillsList();
-					skillSelector.getSkillModel().fireTableDataChanged();
+		
 					refreshSkills();
 				}
 			}
@@ -157,6 +163,7 @@ public class Admin extends JPanel implements ActionListener
 			if (!successful)
 			{
 				JOptionPane.showMessageDialog(null, "Skill is already in use, so cannot be deleted?");
+				 
 			}
 		}
 
@@ -165,14 +172,18 @@ public class Admin extends JPanel implements ActionListener
 			Skill newSkill = skillSelector.getSelectedSkill();
 			if (newSkill.getSkillID() == -1)
 			{
+
 				int dialogResult = JOptionPane.showConfirmDialog(null,
 						"Are you sure you want to add skill?" + newSkill.getSkillName());
 
 				if (dialogResult == JOptionPane.YES_NO_OPTION)
 				{
-					transaction = new Transaction("AddSkill", newSkill.getSkillName()); // setup transaction
-					transaction = commonStuff.getClient().sendTransaction(transaction); // sent transaction
+					transaction = new Transaction("AddSkill", newSkill.getSkillName()); // setup
+					// transaction
+					transaction = commonStuff.getClient().sendTransaction(transaction); // sent
+																						// transaction
 					successful = (boolean) transaction.getObject();
+					 
 				}
 
 				if (!successful)
@@ -182,26 +193,11 @@ public class Admin extends JPanel implements ActionListener
 				else
 				{
 					JOptionPane.showMessageDialog(null, "Skill successfully added");
-					
-					populateSkillsList();
-					skillSelector.getSkillModel().fireTableDataChanged();
 					refreshSkills();
+				 
 				}
+
 			}
 		}
 	}
-	
-//	public static boolean checkDuplicate(ArrayList list) 
-//	{
-//		 HashSet set = new HashSet();
-//		 for (int i = 0; i < list.size(); i++)
-//		 {
-//		  boolean value = set.add(list.get(i));
-//		  if (value == false)
-//			  {
-//			  	return value;
-//			  }
-//		 }
-//		 return true;
-//		}
 }
